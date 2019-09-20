304e5fdaddc70abe6c33526694b347637bf6c85b
ozubergs <34900230+ozubergs@users.noreply.github.com>
2019-09-20 01:16:00 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+//Init App
+const app = express();
+
+app.use("/public", express.static(__dirname + '/public')); //static file
+app.set("view engine", "pug"); //template engine pug
+
+//Home route
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+//Menu route
+app.get('/menu', (req, res) => {
+  res.render("menu")
+});
+
+
+//Start server
+app.listen(3000, function() {
+  console.log("Server listening on port 3000.")
+});
